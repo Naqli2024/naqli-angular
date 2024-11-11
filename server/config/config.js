32b3451a4da24e7ba94c4ebd8f207fe8c411ff03
naqli2024 <naqli2024@gmail.com>
2024-11-11 16:32:21 +0530

    config.js changes

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 const connectDb = async () => {
-  // Use the correct MongoDB connection string format for your MongoDB Atlas instance.
-  const connectionString = `mongodb://${process.env.MONGO_USERNAME}:${encodeURIComponent(process.env.MONGO_PASSWORD)}@10.0.2.28:27017/${process.env.DATABASE_NAME}?authSource=admin&useNewUrlParser=true&useUnifiedTopology=true`;
-
+  const connectionString = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@10.0.2.28:27017/${process.env.DATABASE_NAME}?authSource=admin`;
+  
   try {
     await mongoose.connect(connectionString);
     console.log("Database Connected");
