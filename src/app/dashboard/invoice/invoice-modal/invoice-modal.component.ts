d549cc1842cdb14fc4f12daf04e6f2dc2bc2a660
naqli2024 <naqli2024@gmail.com>
2025-07-31 13:49:55 +0530

    invoice modal component

--- src/app/dashboard/invoice/invoice-modal/invoice-modal.component.ts
+++ src/app/dashboard/invoice/invoice-modal/invoice-modal.component.ts
@@ -3,25 +3,29 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { QRCodeModule } from 'angularx-qrcode';
+import { BookingService } from '../../../../services/booking.service';
 
 @Component({
   selector: 'app-invoice-modal',
   standalone: true,
-  imports: [CommonModule, TranslateModule, QRCodeModule,],
+  imports: [CommonModule, TranslateModule, QRCodeModule],
   templateUrl: './invoice-modal.component.html',
   styleUrl: './invoice-modal.component.css',
 })
 export class InvoiceModalComponent implements OnInit {
   qrCodeData: any = null;
   isModalOpen: boolean = false;
+  booking: any = null;
 
-  constructor(private route: ActivatedRoute, private cd: ChangeDetectorRef) {}
+  constructor(
+    private route: ActivatedRoute,
+    private cd: ChangeDetectorRef,
+    private bookingService: BookingService
+  ) {}
 
   ngOnInit(): void {
-    // Parse query parameters from the URL
     this.route.queryParams.subscribe((params) => {
       if (params['InvoiceId']) {
-        // Populate the QR code data
         this.qrCodeData = {
           InvoiceId: params['InvoiceId'],
           InvoiceDate: params['InvoiceDate'],
@@ -35,7 +39,22 @@ export class InvoiceModalComponent implements OnInit {
         };
 
         this.isModalOpen = true;
-        this.cd.detectChanges();
+
+        if (params['bookingId']) {
+          this.bookingService
+            .getBookingsByBookingId(params['bookingId'])
+            .subscribe(
+              (response) => {
+                this.booking = response.data;
+                this.cd.detectChanges();
+              },
+              (error) => {
+                console.error('Failed to fetch booking details:', error);
+              }
+            );
+        } else {
+          this.cd.detectChanges(); // Only if no bookingId is present
+        }
       }
     });
   }
